d60695fc6a4fdf880400b58ede1068ed75479f10
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2024-06-04 09:03:08 +0100

    Implement pure functions

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -9,13 +9,21 @@ contract ERC20 {
         uint256 _value
     );
 
-    function name() public pure returns (string memory) {}
+    function name() public pure returns (string memory) {
+        return "KOOK KOO ROO KOOOOO";
+    }
 
-    function symbol() public pure returns (string memory) {}
+    function symbol() public pure returns (string memory) {
+        return "KOOK";
+    }
 
-    function decimals() public pure returns (uint8) {}
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
 
-    function totalSupply() public pure returns (uint256) {}
+    function totalSupply() public pure returns (uint256) {
+        return 1000 ether;
+    }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {}
 
