b26b073d42cb17d18adf2df6d2c6d469d1cb38e6
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2024-06-09 06:13:17 +0100

    Update test for AutoToken

--- test/Unit Test/AutoTokenTest.t.sol
+++ test/Unit Test/AutoTokenTest.t.sol
@@ -2,12 +2,24 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {AutoToken} from "../src/AutoToken.sol";
+import {AutoToken} from "../../src/AutoToken.sol";
 
 contract AutoTokenTest is Test {
+    uint256 initialSupply = 1000 ether;
+    address OWNER = makeAddr("owner");
+    AutoToken autoToken;
+
     function setUp() public {
-        vm.startBroadcast();
-        AutoToken autoToken = new AutoToken(1000 ether);
+        vm.startBroadcast(OWNER);
+        autoToken = new AutoToken(initialSupply);
         vm.stopBroadcast();
+        vm.deal(OWNER, 1 ether);
+    }
+
+    //AutoToken
+    function testInitialSupplyIsMintedForMsgSender() public view {
+        uint256 ownerBalance = autoToken.balanceOf(OWNER);
+        console.log(ownerBalance);
+        assert(ownerBalance > 0);
     }
 }
